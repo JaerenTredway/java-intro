801ba9e485c8baa8e54843fb245e5f493140fb41
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-08-28 12:16:16 -0600

    Changed scanner name and closed it.

--- cs_152_lab_2/src/GuessNumber.java
+++ cs_152_lab_2/src/GuessNumber.java
@@ -7,16 +7,17 @@ import java.util.Scanner;
 public class GuessNumber {
 
     /**
-     * This program plays a game with the user by randomly generating a number from 1 to 10
+     * This main() method plays a game with the user by randomly generating a number from 1 to 10
      *  and making the user try to guess it.
      * @param args Command line arguments are ignored.
      */
     public static void main(String[] args) {
         System.out.println("What is your name?");
-        Scanner userInput = new Scanner(System.in);
-        String userName = userInput.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+
         System.out.println(userName + ", please pick a number between 1 and 10");
-        int guess = userInput.nextInt();
+        int guess = scanner.nextInt();
         System.out.println("You guessed " + guess);
 
         int randomNum = (int)(Math.random() * 10) + 1;
@@ -27,5 +28,6 @@ public class GuessNumber {
         } else {
             System.out.println("Better luck next time, " + userName);
         }
+        scanner.close();
     } //End of main()
 } // End of class GuessNumber
